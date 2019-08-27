bc10ca8134b60fae2a7acd746e7302329e061940
gaojme <15822985265@163.com>
2019-08-27 18:15:30 +0800

    first

---
+++ eureka-consumer/src/main/java/com/example/controller/TestController.java
@@ -0,0 +1,45 @@
+package com.example.controller;
+
+import com.example.entity.User;
+import com.example.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 〈一句话功能简述〉<br>
+ * 〈〉
+ *
+ * @author Administrator
+ * @ClassName TestController
+ * @Description
+ * @create 2019/8/27/0027 16:45
+ * @since 1.0.0
+ * 备注:写这段代码的时候，只有上帝和我知道它是干嘛的。现在，只有上帝知道。
+ */
+@RestController
+public class TestController {
+    private Logger log = LoggerFactory.getLogger(TestController.class);
+
+    @Autowired
+    private UserService userService;
+
+
+    @GetMapping(value = "/user/{id}")
+    public User getUser(@PathVariable Long id) {
+        return userService.getUser(id);
+    }
+
+
+}
